9cdae2534c05ef4b6623350e17f00e572fb08050
mohammadhosseinbagheri <mohammadhosseinbagheri>
2021-11-30 09:52:24 +0330

    create header component

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Header = () => {
+  return (
+    <header className="row poster_header__container d-flex align-items-center m-0 p-0 px-2 ">
+      <p className=" text-center poster_p__menu_item ">تست فنی صبا ایده</p>
+    </header>
+  );
+};
+export default Header;
